35ef36fdd614ea574b64b68e820f100d3cb3b45b
Maxim Loginov <zeliboba@mail.ru>
2011-03-22 23:36:28 +0100

    static pages and simple layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 TagParts::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/about"
+
   resources :tags
 
   resources :parts
@@ -54,7 +60,7 @@ TagParts::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
