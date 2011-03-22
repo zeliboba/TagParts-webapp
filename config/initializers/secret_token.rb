872733c4ea4505a15deb9d515cdc50e47cb6c3bc
Maxim Loginov <zeliboba@mail.ru>
2011-03-22 16:12:52 +0100

    just created new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TagParts::Application.config.secret_token = '7432c9c206c61abab50090e67185917918323e30e2848b67fa6bf2820babb7d1ab78566099102bf7543f477414b9a8564d1f3f2449679823f6a5c0343d197670'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TagParts::Application.config.session_store :cookie_store, :key => '_TagParts_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TagParts::Application.config.session_store :active_record_store
