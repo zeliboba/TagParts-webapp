5bf22dfc7388e6eed1c1d540f125e44e260287be
Maxim Loginov <zeliboba@mail.ru>
2011-05-03 21:46:57 +0200

    regenerate tags: no reference to parts, spec tests

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,2 @@
 class Tag < ActiveRecord::Base
-  belongs_to :tag
 end
--- db/migrate/20110322212548_create_tags.rb
+++ db/migrate/20110322212548_create_tags.rb
@@ -2,7 +2,6 @@ class CreateTags < ActiveRecord::Migration
   def self.up
     create_table :tags do |t|
       t.string :word
-      t.integer :part_id
 
       t.timestamps
     end
