b7de494937c143019fe83cba8ce0e64a163869fe
Maxim Loginov <zeliboba@mail.ru>
2011-05-09 18:12:59 +0200

    use model methods for source representation

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,9 @@
 class Source < ActiveRecord::Base
   has_many :parts, :dependent => :destroy
+
+  def headline
+    file = info.split('</td>')[1].reverse.truncate(60).reverse
+    word = info.split('</td>')[3].split('"')[1]
+    return "#{word} from #{file}"
+  end
 end
