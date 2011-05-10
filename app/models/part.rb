f0730d0f12334724a0cf8a2a8810c58bbd6987d8
Maxim Loginov <zeliboba@mail.ru>
2011-05-11 00:19:23 +0200

    use hpricot for html parsing id parts headline

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,25 @@
+require 'hpricot'
+
 class Part < ActiveRecord::Base
   belongs_to :source
   has_and_belongs_to_many :tags
+
+  def headline
+    ncontext = 2
+    doc = Hpricot(content).at('table')
+    # this might help to reduce the width of the table
+    #doc.set_attribute('width', '75%')
+    # get tr and td of the line with the word
+    trtd = doc.at('//font[@color="#0000ff"]').css_path.gsub(/\D/,' ').split()
+    tr = trtd[0].to_i
+    td = trtd[1].to_i
+    # remove row except the line and context
+    doc.search("tr:gt(#{tr + ncontext})").remove
+    doc.search("tr:lt(#{tr - ncontext})").remove
+    # remove some columns
+    doc.search("td:eq(3)").remove
+    doc.search("td:lt(2)").remove
+    return "#{doc.to_html}"
+  end
+
 end
