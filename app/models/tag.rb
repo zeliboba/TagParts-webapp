6275e206d9fe4ee716bc2d3d83bff9d1ae45402c
Maxim Loginov <zeliboba@mail.ru>
2011-05-11 23:11:39 +0200

    tags are ordered by default, show them in parts

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,7 @@ class Tag < ActiveRecord::Base
   has_and_belongs_to_many :parts
   validates_uniqueness_of :word
   validates_length_of :word, :minimum => 2, :too_short => "please enter at least %d character"
+  default_scope :order => "word"
 
   def to_label
     "#{word}"
