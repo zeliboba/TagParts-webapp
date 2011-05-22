b618dcd67850dd066bfab28aa49f300638625582
Maxim Loginov <zeliboba@mail.ru>
2011-05-22 18:38:39 +0200

    discard changes before update

--- update.bat
+++ update.bat
@@ -3,6 +3,7 @@ copy db\development.sqlite3 db\development.%date:~6,4%%date:~0,2%%date:~3,2%_%ti
 
 rem use 'call git' since git is a batch file in Msysgit 
 rem http://stackoverflow.com/questions/5077254/reading-git-commands-using-batch-bat-file
+call git checkout -- .
 call git pull                                            
 
 call bundle install
