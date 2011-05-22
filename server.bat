817ff440bb45d83cd1bef6d44e5bdfa04753a1cf
Maxim Loginov <zeliboba@mail.ru>
2011-05-22 18:27:46 +0200

    batch scripts to run server and update

---
+++ server.bat
@@ -0,0 +1,6 @@
+@echo ##########################################
+@echo #   Running server for TagParts          #
+@echo #   to work go to http://localhost:3000  #
+@echo #   to stop press Ctrl-C or Alt-f4       #
+@echo ##########################################
+rails server
---
+++ update.bat
@@ -0,0 +1,9 @@
+copy db\development.sqlite3 db\development.%date:~6,4%%date:~0,2%%date:~3,2%_%time:~0,2%24%time:~3,2%%time:~6,2%
+@echo database backed up 
+
+rem use 'call git' since git is a batch file in Msysgit 
+rem http://stackoverflow.com/questions/5077254/reading-git-commands-using-batch-bat-file
+call git pull                                            
+
+call bundle install
+call rake db:migrate
