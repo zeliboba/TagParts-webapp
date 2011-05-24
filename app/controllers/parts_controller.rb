4d308002001db0243cd26879ae2ee93aac54a59d
Maxim Loginov <zeliboba@mail.ru>
2011-05-25 00:41:04 +0200

    AND condition now works, did not test a lot
    from http://stackoverflow.com/questions/5835696/rails-has-many-through-associations-find-with-and-condition-not-or-condition

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -7,7 +7,7 @@ class PartsController < ApplicationController
   def index
     @parts = Part
     @parts = @parts.where(:source_id => params[:source_id]) if params[:source_id]
-    @parts = @parts.joins(:tags).where("tags.id" => params[:tag_id]) if params[:tag_id]
+    @parts = @parts.joins(:tags).where("tags.id" => params[:tag_id]).group("parts.id").having("count(*) = #{params[:tag_id].size}") if params[:tag_id]
     @parts = @parts.order("created_at").page(params[:page])
 
     @tag_ids = []
