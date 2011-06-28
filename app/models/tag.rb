c8f6ddf3bc04ec0b192dc9332ee3c5a59a7e24a1
Maxim Loginov <loginov@nrg.eu>
2011-06-28 16:23:16 +0200

    one-to-many relation for category-tag
    rails generate migration AddCategoryToTags category_id:integer

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,7 @@ class Tag < ActiveRecord::Base
   validates_uniqueness_of :word
   validates_length_of :word, :minimum => 2, :too_short => "please enter at least %{count} character"
   default_scope :order => "word"
+  belongs_to :category
 
   def to_label
     "#{word}"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110628113109) do
+ActiveRecord::Schema.define(:version => 20110628121052) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20110628113109) do
     t.string   "word"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
 end
